b9e3151c186f1c09be29ab21fb2a8c2a19f9a407
Rishabh dubey <153626005+Rishabh1-dubey@users.noreply.github.com>
2024-05-04 13:52:45 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,12 @@ import { addUser, removeUser } from "../utils/userSlice";
 import { toggleGptSerachView } from "../utils/gptSlice"; 
 import { SUPPORTED_LANGUAGES } from "../utils/constants";
 import lang from "../utils/languageConstants";
+import { changeLanguage } from "../utils/configSlice";
 const Header = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
-  const dispatch = useDispatch();
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
   const handleSignout = () => {
     signOut(auth)
@@ -50,31 +52,45 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
-  return (
-    <div className="absolute  w-screen px-8 py-2 bg-gradient-to-b from-black  z-10 flex justify-between">
-      <img className="w-44" src={items} alt="Netflix logo" />
-      {user && (
-        <div className="flex p-4 gap-1">
-          <select className="  p-2 m-2 bg-gray-700 text-white rounded-lg ">
-            {SUPPORTED_LANGUAGES.map((lang) => (
-              <option
-                className="pt-5"
-                key={lang.identifier}
-                value={lang.identifier}
-              >
-                {lang.name}
-              </option>
-            ))}
-          </select>
-          <button
-            className="py-2 px-2 mx-4 cursor-pointer text-white bg-yellow-500 rounded-md"
-            onClick={handleGptSearch}
-          >
-            Search GPT
-          </button>
+ const handleGptSearchClick = () => {
+    //toggle dispatch an action
+    dispatch(toggleGptSerachView());
+  };
 
+  const handleLanguageChange = (e) => {
+    // console.log(e.target.value);
+    dispatch(changeLanguage(e.target.value));
+  };
+
+  return (
+    <div className="absolute  w-screen px-8 py-2 bg-gradient-to-b from-black  z-10 flex flex-col md:flex-row justify-between">
+      <img className="w-44 mx-auto md:mx-0" src={items} alt="Netflix logo" />
+      {user && (
+        <div className="flex p-2 justify-between">
+          {showGptSearch && (
+            <select
+              className="  p-2 m-2 bg-gray-700 text-white rounded-lg "
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option
+                  className="pt-5"
+                  key={lang.identifier}
+                  value={lang.identifier}
+                >
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="py-2 px-4 mx-4  my-2 cursor-pointer text-white bg-yellow-500 rounded-md"
+            onClick={handleGptSearchClick}
+          >
+            {showGptSearch ? "Home Page" : "Search GPT"}
+          </button>
           <img
-            className="h-[45px] w-[46px]"
+            className=" hidden md:block h-[45px] w-[46px]"
             src={user?.photoURL}
             alt="icon-logo"
           />
@@ -86,5 +102,4 @@ const Header = () => {
     </div>
   );
 };
-
 export default Header;
