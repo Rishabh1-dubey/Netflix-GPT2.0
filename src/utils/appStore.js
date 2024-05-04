6418a9a4c0ee224b72d858a99f16236a263390ea
Rishabh dubey <153626005+Rishabh1-dubey@users.noreply.github.com>
2024-05-04 14:01:57 +0530

    Update appStore.js

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,12 +1,16 @@
 import {configureStore} from "@reduxjs/toolkit";
 import userReducer  from "./userSlice";
 import movieReducer  from"./movieSlice" ; 
+import gptReducer from "./gptSlice";
+import configReducer from "./configSlice"
 
 // userSlice.reducer;  isko ko useReducer ki trh import kiya gya haiii
 const appStore = configureStore({
     reducer:{
         user: userReducer,
         movies:movieReducer,
+        gpt: gptReducer,
+        config : configReducer,
         //bhai tune idhr movie likha tha iske liye tera mainconatainer render nhi ho rha tha  idhr movies rheaga tbhi redux store re render ho tera movie
 
     },
