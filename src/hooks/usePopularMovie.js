96929037ec3bcfc6433815813de695cdc71d45e8
Rishabh1-dubey <rishabhdubey006@gmail.com>
2024-05-04 21:52:43 +0530

    fixing production issus

--- src/hooks/usePopularMovie.js
+++ src/hooks/usePopularMovie.js
@@ -1,12 +1,11 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addPopularMovies } from "../utils/movieSlice";
 
 const usePopularMovie = () => {
   //Fetch Data from  TMDB API and update storee
   const dispatch = useDispatch();
-
   const popularMovies=useSelector(
   (store)=>store.movies.popularMovies
 );
