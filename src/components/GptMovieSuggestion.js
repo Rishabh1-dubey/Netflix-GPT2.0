b0608e8af7480270d1957ee45add78f3e6d84608
Rishabh dubey <153626005+Rishabh1-dubey@users.noreply.github.com>
2024-05-04 13:56:11 +0530

    adding movie MovieSuggestion

--- src/components/GptMovieSuggestion.js
+++ src/components/GptMovieSuggestion.js
@@ -1,9 +1,24 @@
-
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
 
 const GptMovieSuggestion = () => {
-    return (
-      <div>GptMovieSuggestion</div>
-    )
+  const { movieNames, movieResults } = useSelector((store) => store.gpt);
+  if (!movieNames) return null;
+  if (!Array.isArray(movieNames) || movieNames.length === 0) {
+    return null;
   }
-  
-  export default GptMovieSuggestion
+  return (
+    <div className="p-4 m-4 bg-black text-white bg-opacity-70">
+      <div>
+        {movieNames?.map((movie, index) => (
+          <MovieList 
+          key={movie} 
+          title={movie}
+           movies={movieResults?.[index]} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default GptMovieSuggestion;
